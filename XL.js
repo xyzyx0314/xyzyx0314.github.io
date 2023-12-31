b919ef0d6a059a88f00f72f60eb968848680ab4f
陈科舟 <95153748+xyzyx0314@users.noreply.github.com>
2023-12-31 19:37:30 +0800

    1

---
+++ XL.js
@@ -0,0 +1,63 @@
+function loadNoteXL()
+{
+  load();
+  xmlhttp.open("GET","html/note/noteXL.html",true);
+  xmlhttp.send();
+  xmlhttp2.open("GET","html/note/note_list.html",true);
+  xmlhttp2.send();
+}
+function loadXL20230621()
+{
+  load();
+  xmlhttp.open("GET","html/XL/XL20230621.html",true);
+  xmlhttp.send();
+  
+  xmlhttp2.open("GET","html/XL/XL20230621_list.html",true);
+  xmlhttp2.send();
+}
+function loadXL20230622()
+{
+  load();
+  xmlhttp.open("GET","html/XL/XL20230622.html",true);
+  xmlhttp.send();
+  
+  xmlhttp2.open("GET","html/XL/XL20230622_list.html",true);
+  xmlhttp2.send();
+}
+function loadXL20230623()
+{
+  load();
+  xmlhttp.open("GET","html/XL/XL20230623.html",true);
+  xmlhttp.send();
+  
+  xmlhttp2.open("GET","html/XL/XL20230623_list.html",true);
+  xmlhttp2.send();
+}
+function loadXL20230624()
+{
+  load();
+  xmlhttp.open("GET","html/XL/XL20230624.html",true);
+  xmlhttp.send();
+  
+  xmlhttp2.open("GET","html/XL/XL20230624_list.html",true);
+  xmlhttp2.send();
+}
+function loadXL20230625()
+{
+  load();
+  xmlhttp.open("GET","html/XL/XL20230625.html",true);
+  xmlhttp.send();
+  
+  xmlhttp2.open("GET","html/XL/XL20230625_list.html",true);
+  xmlhttp2.send();
+}
+
+function loadJT20230717()
+{
+  load();
+  xmlhttp.open("GET","html/XL/JT20230717.html",true);
+  xmlhttp.send();
+  
+  xmlhttp2.open("GET","html/XL/JT20230717_list.html",true);
+  xmlhttp2.send();
+}
